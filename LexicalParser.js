67e66bb77aca145da1971612f6fcaa3ea2fa956e
winter <csf178@gmail.com>
2022-08-29 13:37:24 +0800

    feat:editor & compile

---
+++ LexicalParser.js
@@ -0,0 +1,73 @@
+﻿var lex = 
+{
+    InputElement:"<WhiteSpace>|<LineTerminator>|<Comment>|<Keyword>|<Identifier>|<NumericLiteral>|<Punctuator>", 
+    WhiteSpace:/[\t\v\f\u0020\u00A0\u1680\u180E\u2000-\u200A\u202F\u205f\u3000\uFEFF]/, 
+    LineTerminator:/[\n\r\u2028\u2029]/, 
+    Comment:"<SingleLineComment>|<MultiLineComment>", 
+    SingleLineComment:/\/\/[^\n\r\u2028\u2029]*/, 
+    MultiLineComment:/\/\*(?:[^*]|\*[^\/])*\*?\*\//, 
+    Keyword:/const(?![_$a-zA-Z0-9])/,
+    Identifier:/[_$a-zA-Z][_$a-zA-Z0-9]*/, 
+    Punctuator:/=>|=|\(|\)|;/, 
+    NumericLiteral:/(?:0[xX][0-9a-fA-F]*|\.[0-9]+|(?:[1-9]+[0-9]*|0)(?:\.[0-9]*|\.)?)(?:[eE][+-]{0,1}[0-9]+)?(?![_$a-zA-Z0-9])/, 
+};
+function XRegExp(xregexps, rootname, flag)
+{
+    var expnames = [rootname];
+    function buildRegExp(source)
+    {
+        var regexp = new RegExp(source.replace(/<([^>]+)>/g, 
+            function(all, expname)
+            {
+                if(!xregexps[expname])
+                    return "";
+                expnames.push(expname);
+                if(xregexps[expname]instanceof RegExp)
+                    return "(" + xregexps[expname].source + ")";
+                return "(" + buildRegExp(xregexps[expname]).source + ")";
+            }), flag);
+        return regexp;
+    }
+    var regexp = buildRegExp(xregexps[rootname]);
+    this.exec = 
+        function(string)
+        {
+            var matches = regexp.exec(string);
+            if(matches == null)
+                return null;
+            var result = new String(matches[0]);
+            for(var i = 0;i < expnames.length;i++)
+                if(matches[i])
+                    result[expnames[i]] = matches[i];
+            return result;
+        };
+    Object.defineProperty(this, "lastIndex", 
+    {
+        "get":
+        function()
+        {
+            return regexp.lastIndex;
+        }, "set":
+        function(v)
+        {
+            regexp.lastIndex = v;
+        }
+        
+    });
+}
+function* parseLex(source)
+{
+    let inputElement = new XRegExp(lex, "InputElement", "g");
+    let lastIndex = 0
+    while(true) {
+        let token = inputElement.exec(source);
+        if(!token)
+            return null;
+        if(token && inputElement.lastIndex - lastIndex > token.length)
+        {
+            throw new SyntaxError("Unexpected token ILLEGAL");
+        }
+        lastIndex = inputElement.lastIndex;
+        yield token;
+    }
+}
---
+++ SyntacticalParser.js
@@ -0,0 +1,153 @@
+﻿void function (){
+    var rules = 
+    {
+        "Expression":[["NumericLiteral"], ["Identifier"], ["Function"], ["Expression", "(", "Expression", ")"]], 
+        "Function":[["Identifier", "=>", "Expression"]],
+        "Statement":[["Expression", ";"], ["const", "Identifier", "=", "Expression", ";"]], 
+        "Program":[["SourceElements"]], 
+        "SourceElements":[["Statement"], ["SourceElements", "Statement"]], "SourceElement":[["Statement"], ["FunctionDeclaration"]]
+    };
+    function Symbol(symbolName, token)
+    {
+        this.name = symbolName;
+        this.token = token;
+        this.childNodes = [];
+        this.toString = 
+            function(indent)
+            {
+                if(!indent)
+                    indent = "";
+                if(this.childNodes.length == 1)
+                    return this.childNodes[0]. toString (indent);
+                var str = indent + this.name + (this.token != undefined && this.name != this.token ? ":" + this.token:"") + "\n";
+                for(var i = 0;i < this.childNodes.length;i++)
+                    str += this.childNodes[i]. toString (indent + "    ");
+                return str;
+            };
+    }
+    function SyntacticalParser()
+    {
+        var currentRule;
+        var root = {
+            Program:"$"
+        };
+        var hash = {};
+        function visitNode(node)
+        {
+            hash[JSON.stringify(node)] = node;
+            node.$closure = true;
+            var queue = Object.getOwnPropertyNames(node);
+            while(queue.length)
+            {
+                var symbolName = queue.shift();
+                if(!rules[symbolName])
+                    continue;
+                rules[symbolName].forEach(
+                    function(rule)
+                    {
+                        if(node[symbolName].$lookahead && node[symbolName].$lookahead.some(
+                            function(e)
+                            {
+                                return e == rule[0];
+                            }))
+                            return;
+                        if(!node[rule[0]])
+                            queue.push(rule[0]);
+                        var rulenode = node;
+                        var lastnode = null;
+                        rule.forEach(
+                            function(symbol)
+                            {
+                                if(!rulenode[symbol])
+                                    rulenode[symbol] = {};
+                                lastnode = rulenode;
+                                rulenode = rulenode[symbol];
+                            });
+                        if(node[symbolName].$lookahead)
+                            node[rule[0]].$lookahead = node[symbolName].$lookahead;
+                        if(node[symbolName].$div)
+                            rulenode.$div = true;
+                        rulenode.$reduce = symbolName;
+                        rulenode.$count = rule.filter(function(e) {
+                            return!e.match(/\[([^\]]+)\]/);
+                        }).length;
+                    });
+            }
+            for(var p in node)
+            {
+                if(typeof node[p] != "object" || p.charAt(0) == "$" || node[p].$closure)
+                    continue;
+                if(hash[JSON.stringify(node[p])])
+                    node[p] = hash[JSON.stringify(node[p])];
+                else
+                {
+                    visitNode(node[p]);
+                }
+            }
+        }
+        visitNode(root);
+        var symbolStack = [];
+        var statusStack = [root];
+        var current = root;
+        this.insertSymbol = 
+            function insertSymbol(symbol, haveLineTerminator = false)
+            {
+                while(!current[symbol.name] && current["$reduce"])
+                {
+                    var count = current["$count"];
+                    var newsymbol = new Symbol(current["$reduce"]);
+                    while(count--)
+                        newsymbol.childNodes.push(symbolStack.pop()), statusStack.pop();
+                    current = statusStack[statusStack.length - 1];
+                    this.insertSymbol(newsymbol);
+                }
+                if(!current[symbol.name] && current[";"] && current[";"]["$reduce"] && current[";"]["$reduce"] != "EmptyStatement" && (haveLineTerminator || symbol.name == "}"))
+                {
+                    this.insertSymbol(new Symbol(";", ";"));
+                    return this.insertSymbol(symbol);
+                }
+                current = current[symbol.name];
+                symbolStack.push(symbol), statusStack.push(current);
+                if(!current)
+                    throw new Error();
+                return current.$div;
+            };
+        this.reset = function()
+        {
+            current = root;
+            symbolStack = [];
+            statusStack = [root];
+        };
+        Object.defineProperty(this, "grammarTree", 
+        {   
+            "get": function(){
+                try
+                {
+                    while(current["$reduce"])
+                    {
+                        var count = current["$count"];
+                        var newsymbol = new Symbol(current["$reduce"]);
+                        while(count--)
+                            newsymbol.childNodes.push(symbolStack.pop()), statusStack.pop();
+                        current = statusStack[statusStack.length - 1];
+                        this.insertSymbol(newsymbol);
+                    }
+                    if(symbolStack.length > 0 && current[";"])
+                    {
+                        this.insertSymbol(new Symbol(";", ";"));
+                        return this.grammarTree;
+                    }
+                    if(symbolStack.length != 1 || symbolStack[0].name != "Program")
+                        throw new Error();
+                }catch(e)
+                {
+                    throw new SyntaxError("Unexpected end of input");
+                }
+                return symbolStack[0];
+            }
+            
+        });
+    }
+    window.SyntacticalParser = SyntacticalParser;
+    window.SyntacticalParser.Symbol = Symbol;
+}();
